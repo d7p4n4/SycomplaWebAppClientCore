025912362c0f9bf4aa93221b227c3616e94c659e
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-03 09:58:34 +0200

    1.20200403.2

---
+++ SycomplaWebAppClientCore/Ac4yRestServiceClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace SycomplaWebAppClientCore
+{
+    public class Ac4yRestServiceClient
+    {
+        public string GET(string URL, string DATA)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(URL);
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(URL),
+                Content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json"),
+            };
+
+            HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+
+            HttpResponseMessage message = httpClient.SendAsync(request).Result;
+            string result = message.Content.ReadAsStringAsync().Result;
+
+            return result;
+        }
+    }
+}
--- SycomplaWebAppClientCore/Program.cs
+++ SycomplaWebAppClientCore/Program.cs
@@ -9,47 +9,20 @@ namespace SycomplaWebAppClientCore
     {
         static void Main(string[] args)
         {
-            DummyClass dummy = new DummyClass()
-            {
-                Dummy = "test"
-            };
-
-            string URL = "https://service.sycompla.hu/dummy";
-
-            string DATA = JsonConvert.SerializeObject(dummy);
 
             DummyResult dummyResult = new DummyResult();
             try
             {
-
-                HttpClient httpClient = new HttpClient();
-                httpClient.BaseAddress = new Uri(URL);
-                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpRequestMessage request = new HttpRequestMessage()
-                {
-                    Method = HttpMethod.Get,
-                    RequestUri = new Uri(URL),
-                    Content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json"),
-                };
-
-                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
-
-                HttpResponseMessage message = httpClient.SendAsync(request).Result;
-
-                if (message.IsSuccessStatusCode)
-                {
-                }
-                else
-                {
-                    Console.WriteLine("még dolgozz rajta");
-                }
+                string response = new Ac4yRestServiceClient().GET("https://service.sycompla.hu/dummy", "{'Dummy': 'Test'}");
+                Console.WriteLine(response);
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception.StackTrace);
             }
         }
+
+
     }
     
 }
